6e15387f16382da31bfe00456b73e3213c86060f
EZLight777 <antiblasphemystudios@protonmail.com>
2023-02-03 07:16:29 -0500

    update

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,19 +1,31 @@
-import React, {useState} from "react";
+import React from "react"
+import Dropdown from "./Dropdown"
+import Image from "./image"
+import {AbsLogo} from '../styles/images'
+import Header from "./Header"
 
-function Theme(){
-
-const [toggleTheme, setToggleTheme] = useState(Light);
-const onClick = () => setToggleTheme(Dark);
+function Theme(props){
 
 return (
     <div>
-        {toggleTheme ? Light : Dark}
-    </div>
+   <section className="w3-content " >
+      <Dropdown title="Menu" color="black" demo="Menu" classType="w3-button w3-circle w3-black"><Header /></Dropdown>
+<section className="w3-display-container">
+<Image imgSrc={AbsLogo} style={{width:'250px'}}/>
+<article className="w3-display-topleft">
+<Dropdown title={props.title} color="black" demo="News" classType="w3-button w3-circle w3-amber">
+  <p className="w3-tiny w3-opacity-min w3-container w3-rest w3-gray">{props.children}</p>
+</Dropdown>
+
+</article>
+
+</section>
+<footer className="w3-tiny">&copy; AntiBlasphemy Studios 2023</footer>
+
+   </section>
+   </div>
 )
 
 }
-
-const Dark = 'w3-black'
-const Light = 'w3-white'
 
 export default Theme
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Dropdown from '../components/Dropdown'
 import Header from "../components/Header";
+import Theme from "../components/Theme";
 import {AbsLogo} from '../styles/images'
 
 export default function Home() {
@@ -9,21 +10,7 @@ export default function Home() {
 
   return (
     <div>
-   <section className="w3-content " >
-      <Dropdown title="Menu" color="black" demo="Menu" classType="w3-button w3-circle w3-black"><Header /></Dropdown>
-<section className="w3-display-container">
-
-<img className="w3-image" src={AbsLogo} style={{width:'350px'}} />
-<article className="w3-display-left">
-<Dropdown title="News" color="black" demo="News" classType="w3-button w3-circle w3-amber">
-  <h5 className=" w3-opacity-min w3-container w3-gray">jklaj;laj;ljalj;</h5>
-</Dropdown>
-
-</article>
-
-</section>
-  
-   </section>
+   <Theme title="News" >slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.</Theme>
    </div>
   )
 }
